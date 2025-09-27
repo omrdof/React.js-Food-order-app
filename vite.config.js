f6a5f92964462d0a85dd82f9458b141a8db3bf1e
Omar Dof <omrdof@gmail.com>
2025-09-27 15:26:00 +0300

    changed root in vite file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React.js-Food-order-app/', // 👈 required for GitHub Pages
+  root: 'src',                        // 👈 tells Vite source folder
+  build: {
+    outDir: '../dist',                // 👈 put build outside src
+  },
+  base: '/React.js-Food-order-app/',  // 👈 required for GitHub Pages
 })
