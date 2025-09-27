40e0fba7da0a1900ac6dd478f5ad460c1629e95e
Omar Dof <omrdof@gmail.com>
2025-09-27 15:14:10 +0300

    Update config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React.js-Food-order-app/', // 👈 required for GitHub Pages
 })
